a708b5e00f0c6bea8c61c2d6ca0242630fa3c7c9
marina-alekseevna <marina.zmieva@gmail.com>
2022-05-15 19:00:47 +0100

    added scatterplot, edited choropleth

--- financial-markets.py
+++ financial-markets.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 import datetime as dt
 from datetime import datetime, date, time
-from src.charts import makeChoropleth
+from src.charts import makeChoropleth, makeScatterplot
 import plotly.graph_objects as go
 
 from src.utils import countries_cpi_ir_data
@@ -37,21 +37,30 @@ with select_month:
   
 df = pd.read_csv(countries_cpi_ir_data)
 all_countries = tuple(df.name.unique())
+
 with select_countries:
   st.write("\n\n\n")
   symbols = st.multiselect("Choose countries", all_countries, 
     ("UK", "Germany", "Russian Federation", "Japan", "France", "USA", "Australia"))
 
-graph1, graph2 = st.columns((1,1))
+scatter_section_1, trendplots_1 = st.columns(2)
+scatterplot_1 = makeScatterplot(
+  df[(df.year==year) & 
+  (df.month==month) & 
+  (df.name.isin(symbols))], 
+  ("CPI", "InterestRate")
+)
+st.plotly_chart(scatterplot_1)
+choropleth_section_1, choropleth_section_2 = st.columns(2)
 
-with graph1:
+with choropleth_section_1:
   choropleth1=makeChoropleth(df[(df.year == year)&(df.month == month)], -1, 50,'InterestRate', "sunset", 
               "Interest Rate (%)", '<b>Central Bank Policy Rates</b>',
               'Source: BIS: Statistics (https://www.bis.org/statistics/full_data_sets.htm)')
 
 
   st.plotly_chart(choropleth1, use_container_width=True)
-with graph2:
+with choropleth_section_2:
   choropleth2=makeChoropleth(df[(df.year == year)&(df.month == month)], -1, 50,'CPI', "sunset", 
               "CPI (%)", '<b>Consumer Price Index Change</b>',
               'Source: BIS: Statistics (https://www.bis.org/statistics/full_data_sets.htm)')
--- src/charts.py
+++ src/charts.py
@@ -1,5 +1,6 @@
 import plotly.graph_objects as go
 import pandas as pd
+from typing import Tuple, List, Union
 
 
 
@@ -41,15 +42,18 @@ def makeChoropleth(df: pd.DataFrame,
         text=df[f'text_{indicator}'], 
         hovertemplate="%{text}",
         hoverlabel = dict(namelength=0),
-        colorbar_title=colorbar_title))
+        colorbar_title=colorbar_title,
+        colorbar_orientation='h',
+        colorbar_thickness = 15,
+        colorbar_yanchor = "bottom",
+        colorbar_y=-0.2))
 
     fig.update_layout(
-        dragmode=False,
+        dragmode="zoom",
         hoverlabel=dict(
             bgcolor="white",
             font_size=16,
             font_family="Sans-Serif"
-
         ),
 
         title={
@@ -67,16 +71,80 @@ def makeChoropleth(df: pd.DataFrame,
             color='#525252'
         ),
 
-
+        legend = dict(
+            orientation="h"
+        ),
         annotations = [dict(
             x=0,
-            y=-0.05,
+            y=-0.21,
             xref='paper',
             yref='paper',
             text=source,
             showarrow = False,
         )]
     )
+    return fig
 
+def makeScatterplot(df: pd.DataFrame, indicators: Union[Tuple[str, str], List[str]], 
+                   hovertemplate: str="%{text}<br>interest rate: %{y:.2f}%,<br>inflation %{x:.2f}%", 
+                   textposition:str="top right") -> go.Figure:
+    '''
+    Delivers a formatted go.figure scatterplot
+    
+    Args:
+        df (pd.DataFrame): dataframe that contains data for visualisation
+        indicators (Tuple[str, str] or List[str, str]): indicators to be visualised, in (x, y) format
+        hovertemplate (str): template to format hovertemplate output
+        textposition (str): placement of text around the datapoints
+    Returns:
+        go.Figure, a scatterplot with visualised data
+    '''
+    fig= go.Figure(go.Scatter(
+        x=df[indicators[0]],
+        y=df[indicators[1]],
+        text=df["name"],
+        hovertemplate=hovertemplate,
+        name="",  
+        marker=dict(
+                color="#E57A88",
+                opacity=0.8,
+                line=dict(
+                    color='#525252',
+                    width=1),
+                size=20),
 
+        textposition=textposition,
+        mode="markers+text"
+    ))
+
+    fig.update_layout(
+        template="none",
+        grid=dict(xgap=0.05, ygap=0.05),
+        font=dict(color='#525252'),
+        hoverlabel=dict(
+            bgcolor="white",
+            font_size=16,
+            font_family="Sans-Serif"
+        ),
+        title={
+                'text': f"<b>{indicators[0]} vs {indicators[1]}</b>",
+                'y':0.85,
+                'x':0.5,
+                'xanchor': 'center',
+                'yanchor': 'top',
+            },
+        title_font_color='#525252',
+        title_font_size=26
+    )
+
+    fig.update_xaxes(
+            tickangle = 0,
+            title_text = indicators[0],
+            title_font = {"size": 20},
+            title_standoff = 15)
+    fig.update_yaxes(
+            tickangle = 0,
+            title_text = indicators[1],
+            title_font = {"size": 20},
+            title_standoff = 15)
     return fig
